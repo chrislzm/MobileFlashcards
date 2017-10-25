fd976c7bf6b4d7f9eec2831bafc38935e75a8b21
chrislzm <me@chrisleung.com>
2017-10-25 15:12:47 -0700

    Initial commit

---
+++ components/NewQuestion.js
@@ -0,0 +1,12 @@
+import React,  { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class NewQuestion extends Component {
+  render() {
+    return (
+      <View>
+        <Text>NewQuestion component</Text>
+      </View>
+    )
+  }
+}
---
+++ components/Quiz.js
@@ -0,0 +1,12 @@
+import React,  { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class Quiz extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Quiz component</Text>
+      </View>
+    )
+  }
+}
