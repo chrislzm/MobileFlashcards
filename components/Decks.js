9bda1ef5681c1b629b8e608e81313c974cd7c4f8
chrislzm <me@chrisleung.com>
2017-10-25 14:59:09 -0700

    Initial commit

---
+++ components/Decks.js
@@ -0,0 +1,12 @@
+import React,  { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class Decks extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Decks component</Text>
+      </View>
+    )
+  }
+}
---
+++ components/NewDeck.js
@@ -0,0 +1,12 @@
+import React,  { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class NewDeck extends Component {
+  render() {
+    return (
+      <View>
+        <Text>NewDeck component</Text>
+      </View>
+    )
+  }
+}
