fd00986cfdf49074fc40271c8026fba9b8d29fac
chrislzm <me@chrisleung.com>
2017-10-25 20:07:06 -0700

    feat: Setting navigation bar title to the deck title

--- components/IndividualDeck.js
+++ components/IndividualDeck.js
@@ -2,6 +2,15 @@ import React,  { Component } from 'react'
 import { View, Text } from 'react-native'
 
 export default class IndividualDeck extends Component {
+
+  static navigationOptions = ({navigation}) => {
+    console.log(navigation)
+    const { title } = navigation.state.params
+    return {
+      title
+    }
+  }
+
   render() {
     return (
       <View>
