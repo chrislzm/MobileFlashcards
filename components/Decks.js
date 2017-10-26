21a4dd2d1528b0beabebbac0468425c232b7512a
chrislzm <me@chrisleung.com>
2017-10-25 17:22:00 -0700

    feat: Implemented basic list of decks

--- components/Decks.js
+++ components/Decks.js
@@ -1,12 +1,45 @@
 import React,  { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
+import { getDecks } from '../utils/api.js'
+
+function Deck ({ title, questions }) {
+  return (
+    <View key={title}>
+      <Text>{title}</Text>
+      <Text>{questions.length} cards</Text>
+    </View>
+  )
+}
 
 export default class Decks extends Component {
+  renderItem = ({ item }) => {
+    return <Deck {...item}/>
+  }
+
   render() {
+
+    const decks = convertDecksToArrayWithKey(getDecks())
+
     return (
       <View>
-        <Text>Decks component</Text>
+        { !decks && (
+          <Text>No flashcard decks. Please add a new deck!</Text>
+        )}
+        { decks && (
+          <FlatList
+            data={decks}
+            renderItem={this.renderItem}
+          />
+        )}
       </View>
     )
   }
+}
+
+function convertDecksToArrayWithKey(object) {
+  return Object.keys(object).map((key) => {
+    let arrayObj = object[key]
+    arrayObj.key = key
+    return arrayObj
+  })
 }
