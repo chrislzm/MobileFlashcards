bfdb3dfdd6563588ae43496a90bf03220240120b
chrislzm <me@chrisleung.com>
2017-10-25 21:45:13 -0700

    feat: Implemented add new card view functionality

--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,12 +1,52 @@
 import React,  { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import { submitNewQuestion } from '../actions'
+import { validateTextInput } from '../utils/helpers'
 
-export default class NewQuestion extends Component {
+class NewQuestion extends Component {
+  state = {
+    question: '',
+    answer: ''
+  }
+
+  handleTextChange = (input,field) => {
+    this.setState(() => ({
+      [field]: input
+    }))
+  }
+
+  handleSubmit = () => {
+    const { question, answer } = this.state
+    if(validateTextInput(question,'\"Question\"') && validateTextInput(answer,'\"Answer\"')) {
+      const { title } = this.props.navigation.state.params
+      const questionObject = { question, answer }
+      this.props.dispatch(submitNewQuestion(title,questionObject))
+    }
+  }
+
   render() {
+    const { question, answer } = this.state
+
     return (
-      <View>
-        <Text>NewQuestion component</Text>
-      </View>
+      <KeyboardAvoidingView behavior='padding'>
+        <Text>Add Card</Text>
+        <Text>Question:</Text>
+        <TextInput
+          value={question}
+          onChangeText={(input) => this.handleTextChange(input,'question')}
+        />
+        <Text>Answer:</Text>
+        <TextInput
+          value={answer}
+          onChangeText={(input) => this.handleTextChange(input,'answer')}
+        />
+        <TouchableOpacity onPress={this.handleSubmit}>
+          <Text>Submit</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     )
   }
 }
+
+export default connect()(NewQuestion)
