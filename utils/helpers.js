5ad8cef2a8d665edea6e5b58c5f006ad9b8c80a2
chrislzm <me@chrisleung.com>
2017-10-25 21:44:17 -0700

    feat: Added validate text input function with alert modal

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,17 @@
+import { Alert } from 'react-native'
 export function convertObjectToArrayWithKey(object) {
   return Object.keys(object).map((key) => {
     let arrayObj = object[key]
     arrayObj.key = key
     return arrayObj
   })
+}
+
+
+export function validateTextInput(data,name) {
+  if(!data) {
+    Alert.alert('Error',`${name} may not be empty`)
+    return false
+  }
+  return true
 }
