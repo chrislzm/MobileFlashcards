93807fffe45544ea36155a2e28b12d0beaac9af7
chrislzm <me@chrisleung.com>
2017-10-25 19:02:32 -0700

    feat: Implemented API methods to get decks and save decks

--- utils/api.js
+++ utils/api.js
@@ -1,26 +1,13 @@
-export function getDecks() {
-  return ({
-    React: {
-      title: 'React',
-      questions: [
-        {
-          question: 'What is React?',
-          answer: 'A library for managing user interfaces'
-        },
-        {
-          question: 'Where do you make Ajax requests in React?',
-          answer: 'The componentDidMount lifecycle event'
-        }
-      ]
-    },
-    JavaScript: {
-      title: 'JavaScript',
-      questions: [
-        {
-          question: 'What is a closure?',
-          answer: 'The combination of a function and the lexical environment within which that function was declared.'
-        }
-      ]
-    }
-  })
-}
+import { AsyncStorage } from 'react-native'
+
+const DECKS_STORAGE_KEY = "Flashcards:decks"
+
+export const getDecks = (handler) => (
+  AsyncStorage.getItem(DECKS_STORAGE_KEY)
+)
+
+export const saveDeckTitle = (title) => (
+  AsyncStorage.mergeItem(DECKS_STORAGE_KEY,JSON.stringify({
+    [title]: { title }
+  }))
+)
