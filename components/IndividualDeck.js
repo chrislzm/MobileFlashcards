92301f559524fdde315003dc64183ef65bf2d7ae
chrislzm <me@chrisleung.com>
2017-10-25 20:51:50 -0700

    feat: Now displaying deck information and buttons to add card/start quiz

--- components/IndividualDeck.js
+++ components/IndividualDeck.js
@@ -1,21 +1,33 @@
 import React,  { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
 
-export default class IndividualDeck extends Component {
-
-  static navigationOptions = ({navigation}) => {
-    console.log(navigation)
-    const { title } = navigation.state.params
-    return {
-      title
-    }
-  }
+class IndividualDeck extends Component {
 
   render() {
+    const { title, questions, navigation } = this.props
+    const numCards = questions ? questions.length : 0
     return (
       <View>
-        <Text>IndividualDeck component</Text>
+        <Text>{title}</Text>
+        <Text>{numCards} Cards</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('NewQuestion', {title})}>
+          <Text>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Quiz', {title})}>
+          <Text>Start Quiz</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+function mapStateToProps(state, props) {
+  const { title }  = props.navigation.state.params
+  return ({
+    title,
+    questions: state[title].questions
+  })
+}
+
+export default connect(mapStateToProps)(IndividualDeck)
