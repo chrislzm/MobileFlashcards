1590ca67c4e8080edf1fc0e3e2f83010d822275f
chrislzm <me@chrisleung.com>
2017-10-25 21:43:53 -0700

    feat: Added function to add a new card to a deck fix: Set questions to empty array when creating new deck

--- utils/api.js
+++ utils/api.js
@@ -2,12 +2,26 @@ import { AsyncStorage } from 'react-native'
 
 const DECKS_STORAGE_KEY = "Flashcards:decks"
 
-export const getDecks = (handler) => (
+export const addCardToDeck = (title,question) => (
+  getDecks().then((data) => {
+    const decks = JSON.parse(data)
+    let { questions } = decks[title]
+    if(!questions) {
+      questions = []
+    }
+    questions.push(question)
+    return AsyncStorage.mergeItem(DECKS_STORAGE_KEY,JSON.stringify({
+      [title]: { questions }
+    }))
+  })
+)
+
+export const getDecks = () => (
   AsyncStorage.getItem(DECKS_STORAGE_KEY)
 )
 
 export const saveDeckTitle = (title) => (
   AsyncStorage.mergeItem(DECKS_STORAGE_KEY,JSON.stringify({
-    [title]: { title }
+    [title]: { title, questions: [] }
   }))
 )
