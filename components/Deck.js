d833c9c73b31ff1da6aaf52f57eb6a2736303310
chrislzm <me@chrisleung.com>
2017-10-27 14:46:29 -0700

    feat: Added styles refactor: Removed unneeded view wrapper

--- components/Deck.js
+++ components/Deck.js
@@ -1,14 +1,39 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { gray } from '../utils/colors'
 
 export default function Deck (props) {
   const { navigation, title, numCards } = props
   return (
-    <View key={title}>
-      <TouchableOpacity onPress={() => navigation.navigate('IndividualDeck',{ title })}>
-        <Text>{title}</Text>
-        <Text>{numCards} cards</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity key={title} style={styles.deck} onPress={() => navigation.navigate('IndividualDeck',{ title })}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.cards}>{numCards} cards</Text>
+    </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  deck: {
+    flex:1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth:1,
+    borderRadius:10,
+    borderColor:gray,
+    margin:20,
+    marginBottom:5,
+    padding:20
+  },
+  title: {
+    flex:1,
+    fontSize:20,
+    fontWeight:'bold',
+    paddingBottom:10
+  },
+  cards: {
+    flex:1,
+    fontSize:15,
+    color: gray,
+    fontWeight:'bold'
+  }
+})
