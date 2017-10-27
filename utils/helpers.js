31cdb2937d5e3cb2d4709fb89f3e982dc1c41f7d
chrislzm <me@chrisleung.com>
2017-10-26 17:45:33 -0700

    feat: Added functions to manage local notifications

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,8 @@
-import { Alert } from 'react-native'
+import { Alert, AsyncStorage } from 'react-native'
+import { Notifications, Permissions } from 'expo'
+
+const NOTIFICATION_KEY = 'Flashcards:notifications'
+
 export function convertObjectToArrayWithKey(object) {
   return Object.keys(object).map((key) => {
     let arrayObj = object[key]
@@ -14,4 +18,64 @@ export function validateTextInput(data,name) {
     return false
   }
   return true
+}
+
+/*
+ * Notification Helper Functions
+ * Description: The following three functions manage setting and clearing notifications for the Flashcards app
+ * Credit: Source code from UdaciFitness by Tyler McGinnis, with modifications
+ * Reference: https://github.com/udacity/reactnd-UdaciFitness-complete/blob/setLocalNotification/utils/helpers.js
+ */
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+  .then(Notifications.cancelAllScheduledNotificationsAsync())
+}
+
+function createNotification() {
+  return {
+    title: 'Review your flashcards!',
+    body: "👋 don't forget to take a flashcard quiz today!",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true
+    }
+  }
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+  .then(JSON.parse)
+  .then((data) => {
+    // If we have NOT already set up a local notification
+    if(data === null) {
+      Permissions.askAsync(Permissions.NOTIFICATIONS)
+      .then(({ status }) => {
+        if(status === 'granted') {
+          // Clear to make sure we don't set two
+          Notifications.cancelAllScheduledNotificationsAsync()
+
+          let tomorrow = new Date()
+          tomorrow.setDate(tomorrow.getDate() + 1)
+          tomorrow.setHours(20)
+          tomorrow.setMinutes(0)
+
+          Notifications.scheduleLocalNotificationAsync(
+            createNotification(),
+            {
+              time: tomorrow,
+              repeat: 'day'
+            }
+          )
+
+          AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+        }
+      })
+    }
+  })
 }
