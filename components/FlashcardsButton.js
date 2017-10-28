7c6f1f15f13673ae3383d897499fd99972924aec
chrislzm <me@chrisleung.com>
2017-10-27 22:02:42 -0700

    doc: Added description, comments style: Code formatted for readability

--- components/FlashcardsButton.js
+++ components/FlashcardsButton.js
@@ -1,21 +1,41 @@
+/*
+  Flashcards: components/FlashcardsButton.js
+  By Chris Leung
+
+  Description:
+
+  React functional component that implements a button used throughtout this app.
+
+  Props:
+    onPress: <Function> Required. Callback function for the onPress event.
+    children: <String> Required. Contains the text wrapped by this component
+      that will be used as the button text.
+    backgroundColor: <String> Optional. We can pass a different color for the
+      button besides its default.
+*/
+
 import React from 'react'
 import { Text, TouchableOpacity, StyleSheet } from 'react-native'
-import { DEFAULT_BUTTON_COLOR } from '../utils/styles'
-import { white } from '../utils/colors'
 import PropTypes from 'prop-types'
+import { white } from '../utils/colors'
+import { DEFAULT_BUTTON_COLOR } from '../utils/styles'
 
 export default function FlashcardsButton (props) {
   const { onPress, children } = props
-
-  // Update button color if one was passed into props
   let { backgroundColor } = props
+
+    // Set default button color if none was passed in via props
   if(!backgroundColor) {
     backgroundColor = DEFAULT_BUTTON_COLOR
   }
 
   return (
-    <TouchableOpacity style={[styles.button,{backgroundColor}]} onPress={onPress}>
-      <Text style={styles.buttonText}>{children}</Text>
+    <TouchableOpacity
+      style={[styles.button,{backgroundColor}]}
+      onPress={onPress}>
+      <Text style={styles.buttonText}>
+        {children}
+      </Text>
     </TouchableOpacity>
   )
 }
