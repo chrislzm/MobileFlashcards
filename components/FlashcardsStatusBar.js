dfe348cc481c839e9d7eed1515665611fe6d1bfb
chrislzm <me@chrisleung.com>
2017-10-27 20:35:57 -0700

    feat: Added prop-types refactor: Using statusBarHeight from Constants and marking as known issue in README

--- components/FlashcardsStatusBar.js
+++ components/FlashcardsStatusBar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { View, StatusBar, Platform } from 'react-native'
 import { Constants } from 'expo'
+import PropTypes from 'prop-types'
 
 export default function FlashcardsStatusBar ({ backgroundColor, ...props}) {
-  // Expo doesn't correctly set height for iOS; setting manually to 0
-  let height = Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
   return (
-    <View style={{ backgroundColor, height }}>
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
   )
+}
+
+FlashcardsStatusBar.propTypes = {
+  backgroundColor: PropTypes.string.isRequired
 }
