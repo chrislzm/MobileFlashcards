b958006f3f6c2753378e02ac876ef5bbef3eae0b
chrislzm <me@chrisleung.com>
2017-10-27 20:09:03 -0700

    fix: Better status bar height for iOS

--- components/FlashcardsStatusBar.js
+++ components/FlashcardsStatusBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View, StatusBar } from 'react-native'
+import { View, StatusBar, Platform } from 'react-native'
 import { Constants } from 'expo'
 
 export default function FlashcardsStatusBar ({ backgroundColor, ...props}) {
+  // Expo doesn't correctly set height for iOS; setting manually to 0
+  let height = Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
   return (
-    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+    <View style={{ backgroundColor, height }}>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
   )
