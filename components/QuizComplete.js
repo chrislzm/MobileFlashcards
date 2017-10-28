b865632cfcb6f7526ab87ff016d09aeaab0137eb
chrislzm <me@chrisleung.com>
2017-10-27 18:56:58 -0700

    feat: Added styles and button component

--- components/QuizComplete.js
+++ components/QuizComplete.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet} from 'react-native'
+import Button from './Button'
+import { CONTAINER, MEDIUM_FONT, LARGE_FONT } from '../utils/styles'
 
 export default function QuizComplete (props) {
   const { navigation, restartQuiz, numCorrect, numQuestions } = props
   return (
-    <View>
-      <Text>Quiz complete!</Text>
-      <Text>Your Score:</Text>
-      <Text>{Math.round(numCorrect*100.0/numQuestions)}%</Text>
-      <TouchableOpacity onPress={restartQuiz}>
-        <Text>Restart Quiz</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>Back to Deck</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Text style={styles.largeFont}>Quiz complete!</Text>
+      <Text style={styles.mediumFont}>Your Score:</Text>
+      <Text style={styles.largeFont}>{Math.round(numCorrect*100.0/numQuestions)}%</Text>
+      <Button
+        onPress={restartQuiz}>
+        Restart Quiz
+      </Button>
+      <Button
+        onPress={() => navigation.goBack()}>
+        Back to Deck
+      </Button>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: CONTAINER,
+  largeFont: LARGE_FONT,
+  mediumFont: MEDIUM_FONT
+})
