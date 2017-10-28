cf8f3123bc78135e10cf2b8c51f30213ab458dfa
chrislzm <me@chrisleung.com>
2017-10-27 17:52:20 -0700

    refactor: Using shared styles and Button component

--- components/IndividualDeck.js
+++ components/IndividualDeck.js
@@ -1,7 +1,10 @@
 import React,  { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { Alert } from 'react-native'
+import { white, purple, gray } from '../utils/colors'
+import { CONTAINER, MEDIUM_FONT, LARGE_FONT } from '../utils/styles'
+import Button from './Button'
 
 class IndividualDeck extends Component {
 
@@ -17,19 +20,34 @@ class IndividualDeck extends Component {
     const { title, questions, navigation } = this.props
     const numCards = questions.length
     return (
-      <View>
-        <Text>{title}</Text>
-        <Text>{numCards} Cards</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('NewQuestion', {title})}>
-          <Text>Add Card</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.startQuiz(numCards,navigation,title)}>
-          <Text>Start Quiz</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <Text style={styles.largeFont}>{title}</Text>
+        <Text style={styles.mediumFont}>{numCards} Cards</Text>
+        <Button
+          color={white}
+          backgroundColor={purple}
+          onPress={() => navigation.navigate('NewQuestion', {title})}>
+          Add Card
+        </Button>
+        <Button
+          color={white}
+          backgroundColor={purple}
+          onPress={() => this.startQuiz(numCards,navigation,title)}>
+          Start Quiz
+        </Button>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: CONTAINER,
+  largeFont: LARGE_FONT,
+  mediumFont: {
+    ...MEDIUM_FONT,
+    color: gray
+  }
+})
 
 function mapStateToProps(state, props) {
   const { title }  = props.navigation.state.params
