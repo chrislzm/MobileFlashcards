b3d46cebbd96a1cb25f50648ee06e90b0dfe0d0f
chrislzm <me@chrisleung.com>
2017-10-28 19:32:59 -0700

    doc: Added description

--- actions/index.js
+++ actions/index.js
@@ -1,3 +1,16 @@
+/*
+  Readable: actions/index.js
+  By Chris Leung
+
+  Description:
+
+  Contains all Redux actions and action types used in the Flashcards app.
+
+  Thunk actions are used to execute "API" calls to the persistent data store and
+  then dispatch other actions only after the requests have completed. Only Thunk
+  actions interact with the "API" in the Readable app.
+*/
+
 import * as DecksAPI from '../utils/api'
 
 export const ADD_NEW_DECK = 'ADD_NEW_DECK'
