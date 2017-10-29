2ecc4e8bff3a37a8c6389a8c2e40faee1560f20f
chrislzm <me@chrisleung.com>
2017-10-28 19:50:16 -0700

    refactor: Removed notifications object, Updated naming for consistency doc: Added description

--- reducers/index.js
+++ reducers/index.js
@@ -1,41 +1,44 @@
+/*
+  Readable: reducers/index.js
+  By Chris Leung
+
+  Description:
+
+  Contains the Redux root reducer used in the Flashcards app.
+*/
+
 import {
   ADD_NEW_DECK,
   ADD_NEW_QUESTION,
-  RESET_DECK_STORE
+  RESET_DECKS_STORE
 } from '../actions'
 
-const DEFAULT_DECK_STATE = { notifications: {}, decks: {} }
+const DEFAULT_DECKS_STATE = {}
 
 // Keys in our state will be days, values will be the metrics for the specific day
-function decks (state = DEFAULT_DECK_STATE, action) {
+function decks (state = DEFAULT_DECKS_STATE, action) {
   const { title, question, questions } = action
   switch(action.type) {
     case ADD_NEW_DECK:
-      return {
-        ...state,
-        ['decks']: {
-          ...state['decks'],
-          [title]: { title, questions }
-        }
-      }
+    return {
+      ...state,
+      [title]: { title, questions }
+    }
     case ADD_NEW_QUESTION:
-      return {
-        ...state,
-        ['decks']: {
-          ...state['decks'],
-          [title]: {
-            ...state['decks'][title],
-            questions: [
-              ...state['decks'][title].questions,
-              question
-            ]
-          }
-        }
+    return {
+      ...state,
+      [title]: {
+        ...state[title],
+        questions: [
+          ...state[title].questions,
+          question
+        ]
       }
-    case RESET_DECK_STORE:
-      return DEFAULT_DECK_STATE
+    }
+    case RESET_DECKS_STORE:
+    return DEFAULT_DECKS_STATE
     default:
-      return state
+    return state
   }
 }
 
