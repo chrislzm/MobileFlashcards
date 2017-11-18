4a72b2ba3c1d90a128ea9705a0b4f85b73752dc8
chrislzm <me@chrisleung.com>
2017-11-17 18:15:23 -0800

    doc: Update description, comments and formatting

--- components/QuizQuestion.js
+++ components/QuizQuestion.js
@@ -1,26 +1,3 @@
-/*
-  Mobile Flashcards: components/QuizQuestion.js
-  By Chris Leung
-
-  Description:
-
-  React Native component that displays a quiz question along with controls to
-  (1) toggle between showing the question and the answer, (2) submit a "correct"
-  answer and (3) submit an  "incorrect" answer.
-
-  Props:
-    title: <String> Required. Contains the title of the deck the user is being
-      quizzed on.
-    questionText: <String> Required. The text of the quiz question.
-    answerText: <String> Required. The text of the quiz question.
-    questionNum: <Integer> Required. The question number (of total questions).
-    numQuestions: <Integer> Required. The total number of questions.
-    handleCorrect: <Function> Required. Callback that handles user submission
-      of a "correct" answer.
-    handleIncorrect: <Function> Required. Callback that handles user submission
-      of an "incorrect" answer.
-*/
-
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import FlashcardsButton from './FlashcardsButton'
@@ -28,6 +5,13 @@ import { styles } from '../utils/styles'
 import { red, green, gray } from '../utils/colors'
 import PropTypes from 'prop-types'
 
+/**
+ * Displays a quiz question and controls to:
+ * (1) toggle between showing the question and the answer
+ * (2) submit a "correct" answer
+ * (3) submit an "incorrect" answer
+ * @author Chris Leung
+ */
 export default class QuizQuestion extends Component {
 
   state = {
@@ -90,11 +74,18 @@ export default class QuizQuestion extends Component {
 }
 
 QuizQuestion.propTypes = {
+  /** Title of the deck the user is being quizzed on */
   title: PropTypes.string.isRequired,
+  /** Quiz question */
   questionText: PropTypes.string.isRequired,
+  /** Quiz answer */
   answerText: PropTypes.string.isRequired,
+  /** Question number */
   questionNum: PropTypes.number.isRequired,
+  /** Total number of questions */
   numQuestions: PropTypes.number.isRequired,
+  /** Callback that handles user submission of a "correct" answer. */
   handleCorrect: PropTypes.func.isRequired,
+  /** Callback that handles user submission of an "incorrect" answer. */
   handleIncorrect: PropTypes.func.isRequired
 }
